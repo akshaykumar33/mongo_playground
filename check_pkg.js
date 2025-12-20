7374a6e78c1cade815fdc5846993bf2c7f70bf0d
AKSHAYKUMAR SHIVAKUMAR PATIL <akshaykumarpatil33@gmail.com>
2025-12-20 16:29:05 +0530

    removed unnecessary files

--- check_pkg.js
+++
@@ -1,8 +0,0 @@
-const pkg = require("react-resizable-panels");
-console.log("Exports:", Object.keys(pkg));
-try {
-  const pkgJson = require("react-resizable-panels/package.json");
-  console.log("Version:", pkgJson.version);
-} catch (e) {
-  console.log("Could not read package.json version");
-}
